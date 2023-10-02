114c0739c92f8157a2a5ba304bc734e37c7783c7
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2023-10-02 17:08:03 -0300

    committt

--- Javascript Testing/aula04-coverage-e2e/api.js
+++ Javascript Testing/aula04-coverage-e2e/api.js
@@ -1,21 +1,21 @@
 const http = require('http')
-const DEFAULT_USER = { username: "ErickWendel", password: "123"}
+const DEFAULT_USER = { username: "ErickWendel", password: "123" }
 const routes = {
     '/contact:get': (request, response) => {
         response.write('contact us page')
         return response.end()
     },
-    '/login:post': async(request, response) => {
+    '/login:post': async (request, response) => {
         // response é um iterator!
         for await (const data of request) {
             const user = JSON.parse(data)
-            if(
+            if (
                 user.username !== DEFAULT_USER.username ||
                 user.password !== DEFAULT_USER.password
             ) {
-              response.writeHead(401)
-              response.write("Logging failed!")   
-              return response.end()
+                response.writeHead(401)
+                response.write("Logging failed!")
+                return response.end()
             }
 
             response.write("Logging has succeeded!")
@@ -32,14 +32,14 @@ const handler = function (request, response) {
     const { url, method } = request
     const routeKey = `${url}:${method.toLowerCase()}`
     const chosen = routes[routeKey] || routes.default
-    
-    response.writeHead(200, { 
-         'Content-Type': 'text/html'
+
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
     })
     return chosen(request, response)
 }
 
 const app = http.createServer(handler)
-                .listen(3000, () => console.log('app running at', 3000))
+    .listen(3000, () => console.log('app running at', 3000))
 
 module.exports = app
